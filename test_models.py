bbe3e70f495926cd3076919934a54b035da9b04a
Anjana6688 <anjanarajasekhar06963@gmail.com>
2026-02-14 22:54:08 +0530

    Create test_models.py

---
+++ test_models.py
@@ -0,0 +1,14 @@
+def test_read_product():
+   # Test reading a product
+def test_update_product():
+   # Test updating a product
+def test_delete_product():
+   # Test deleting a product
+def test_list_all_products():
+   # Test listing all products
+def test_find_by_name():
+   # Test finding product by name
+def test_find_by_category():
+   # Test finding product by category
+def test_find_by_availability():
+   # Test finding product by availability
